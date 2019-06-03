63c7da9b290169a3a2fe6c677358535c95623339
jmpark\pjm <kbtapjm@gmail.clm>
2019-06-04 07:52:27 +0900

    first commit

---
+++ src/main/java/kr/co/pjm/diving/config/EnjoyDivingConfigApplication.java
@@ -0,0 +1,13 @@
+package kr.co.pjm.diving.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoyDivingConfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnjoyDivingConfigApplication.class, args);
+	}
+
+}
